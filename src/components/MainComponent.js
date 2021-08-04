60e7feb7719c2594317863e4a7d6e44ceef301f3
Maitr <maitri795@gmail.com>
2021-08-04 19:17:06 -0400

    header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Cities from './CitiesComponent';
 import PlaceDetail from './PlacedetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { PLACES } from '../Shared/places';
+import '../App.css';
 
 class Main extends Component {
   constructor(props) {
@@ -21,14 +24,11 @@ class Main extends Component {
   render() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Travel To Ontario</NavbarBrand>
-        </div>
-      </Navbar>
+     <Header />
       <Cities places={this.state.places} onClick={(placeId) => this.onPlaceSelect(placeId)} />
       <PlaceDetail
         selectedPlace={this.state.places.filter((place) => place.id === this.state.selectedPlace)[0]} />
+      <Footer />
     </div>
   );
 }
