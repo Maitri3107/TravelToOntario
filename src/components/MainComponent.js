b7270bbca2fbacc0ee26ddc134f2c86f5de90123
Maitr <maitri795@gmail.com>
2021-08-05 12:24:01 -0400

    React router with navbars in header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Cities from './CitiesComponent';
 import PlaceDetail from './PlacedetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { PLACES } from '../Shared/places';
-import '../App.css';
+import {Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
   constructor(props) {
@@ -13,21 +15,25 @@ class Main extends Component {
 
     this.state = {
       places: PLACES,
-      selectedPlace: null
     };
   }
 
-  onPlaceSelect(placeId) {
-    this.setState({selectedPlace: placeId});
-  }
 
   render() {
+
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
   return (
     <div className="App">
      <Header />
-      <Cities places={this.state.places} onClick={(placeId) => this.onPlaceSelect(placeId)} />
-      <PlaceDetail
-        selectedPlace={this.state.places.filter((place) => place.id === this.state.selectedPlace)[0]} />
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Route exact path="/cities" component={() => <Cities places={this.state.places} />} />
+        <Redirect to="/home" />
+      </Switch>
       <Footer />
     </div>
   );
